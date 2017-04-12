3e4224bb831fd88fe3a7aaa6d69355c7c86d14b9
Mitchell Caisse <computers1055@gmail.com>
2017-04-11 22:31:52 -0400

    Updated vs bindings in gupl file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding BeforeBuild='dist' Clean='clean' />
+﻿/// <binding AfterBuild='dist-local' Clean='clean' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
