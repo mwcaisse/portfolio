ad42e5b3fa177d61eb27f698a63a9998f1b50f0a
Mitchell Caisse <computers1055@gmail.com>
2017-04-13 23:13:52 -0400

    Implemented the ContactMessage Controller

---
+++ src/Portfolio.API/Controllers/ContactMessageController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.API.Data;
+using Portfolio.API.Data.Entity;
+using Portfolio.API.Manager;
+using Portfolio.API.ViewModel;
+
+namespace Portfolio.API.Controllers
+{
+
+    [Route("api/contactmessage")]
+    public class ContactMessageController : Controller
+    {
+
+        private readonly ContactMessageManager _contactMessageManager;
+
+        public ContactMessageController(DataContext dataContext)
+        {
+            _contactMessageManager = new ContactMessageManager(dataContext);
+        }
+
+        [HttpPost]
+        public JsonResponse<bool> Add([FromBody] ContactMessage message)
+        {
+            try
+            {
+                _contactMessageManager.Add(message);
+                return new JsonResponse<bool>()
+                {
+                    Data = true,
+                    ErrorMessage = null
+                };
+            }
+            catch (ManagerException e)
+            {
+                return new JsonResponse<bool>()
+                {
+                    Data = false,
+                    ErrorMessage = e.Message
+                };
+            }
+            catch (Exception)
+            {
+                return new JsonResponse<bool>()
+                {
+                    Data = false,
+                    ErrorMessage = "An error occured while processing your request."
+                };
+            }
+        }
+
+    }
+}
